a6ee70e839119e5aa2369b15e5e7f63070af6faf
Rishita77 <118192740+Rishita77@users.noreply.github.com>
2023-12-18 23:40:31 +0530

    Create Dashboard.jsx

---
+++ LoginRegisterDash/Client/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+
+const YearlyAnalysisChart = () => {
+  return (
+    <canvas id="myChart"></canvas>
+  );
+};
+
+const MonthlyAnalysisChart = () => {
+  return (
+    <div id="chart-container">1</div>
+  );
+};
+
+const Dashboard = () => {
+  return (
+    <div>
+      <nav>
+        <div className="nav">
+          {/* Navigation content */}
+          <div className="logo-container">
+            <img className="nav-logo" src={logo} alt="Logo" />
+          </div>
+          {/* ... Other navigation elements */}
+        </div>
+      </nav>
+      <hr />
+      <div className="analysis">
+        <div className="yearly">
+          <div id="y-head">
+            <div id="heading">Yearly Analysis</div>
+            <a href="#" id="show-more">show more</a>
+          </div>
+          <YearlyAnalysisChart />
+        </div>
+        <div className="monthly">
+          <div id="m-head">
+            <div id="heading">Monthly Analysis</div>
+            <a href="#" id="show-more">show more</a>
+          </div>
+          <MonthlyAnalysisChart />
+        </div>
+      </div>
+      {/* ... Other sections */}
+      <footer>
+        <div className="footer-container">
+          {/* Footer content */}
+          <div className="footer-section" id="footer-left-section">
+            {/* ... Footer left section content */}
+          </div>
+          <div className="footer-section" id="footer-right-section">
+            {/* ... Footer right section content */}
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Dashboard;
